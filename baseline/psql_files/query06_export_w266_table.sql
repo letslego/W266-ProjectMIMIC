00e87e82b7cb983e4ad9e23557b847937e9a9f9b
zliendo <zenobiosky@yahoo.com>
2017-07-18 19:04:52 -0400

    export to csv

--- baseline/psql_files/query06_export_w266_table.sql
+++ baseline/psql_files/query06_export_w266_table.sql
@@ -1,1 +1,2 @@
-select * from W266_DISCHARGE_NOTE_ICD9_CODES;
+select HADM_ID, SUBJECT_ID, CHARTDATE, regexp_replace(TEXT, E'[\\n\\r]+', ' ', 'g' ),  ICD9_CODES
+from W266_DISCHARGE_NOTE_ICD9_CODES;
