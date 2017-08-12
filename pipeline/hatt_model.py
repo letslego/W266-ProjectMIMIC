c8f8396238edb263db76fefbe92d1ae0170580b5
zliendo <zenobiosky@yahoo.com>
2017-08-12 22:04:34 +0000

    hatt runs

--- pipeline/hatt_model.py
+++ pipeline/hatt_model.py
@@ -11,7 +11,23 @@ from keras import backend as K
 # * https://github.com/richliao/textClassifier/blob/master/textClassifierHATT.py
 # * https://github.com/philipperemy/keras-attention-mechanism/blob/master/attention_lstm.py
 
-SINGLE_ATTENTION_VECTOR = False
+def conv_layer(input,input_seq_length, num_filters,filter_sizes): 
+    
+    conv_blocks = []
+    for sz in filter_sizes:
+        conv = Convolution1D(filters=num_filters,                         
+                         kernel_size=sz,
+                         padding="valid",
+                         activation="relu",
+                         strides=1)(input)
+        window_pool_size =  input_seq_length  - sz + 1 
+        conv = MaxPooling1D(pool_size=window_pool_size)(conv)  
+        conv = Flatten()(conv)
+        conv_blocks.append(conv)
+
+    #concatenate
+    z = Concatenate()(conv_blocks) if len(conv_blocks) > 1 else conv_blocks[0]
+    return z
 
 def attention_3d_block(inputs, TIME_STEPS):
     # inputs.shape = (batch_size, time_steps, input_dim)
@@ -23,12 +39,8 @@ def attention_3d_block(inputs, TIME_STEPS):
     a = Dense(TIME_STEPS, activation='softmax')(a)
     
 	# (5) s_i: After that, we compute the sentence vector s_i as a weighted sum of the word annotations based on the weights alpha_it.
-    if SINGLE_ATTENTION_VECTOR:
-        a = Lambda(lambda x: K.mean(x, axis=1), name='dim_reduction')(a)
-        a = RepeatVector(input_dim)(a)
     a_probs = Permute((2, 1), name='attention_vec')(a)
-    output_attention_mul = merge([inputs, a_probs], name='attention_mul', mode='mul')
-    print output_attention_mul.shape   
+    output_attention_mul = merge([inputs, a_probs], name='attention_mul', mode='mul') 
     # w266 where is the sum?
     #sum_vector = add(output_attention_mul, name= 'attention_sum')
     #sum_vector = merge( output_attention_mul, name='attention_add', mode='add') 
@@ -39,8 +51,8 @@ def attention_3d_block(inputs, TIME_STEPS):
 
 
 
-def build_gru_att_model(MAX_SENTS, MAX_SENT_LENGTH, 
-                         max_vocab, embedding_dim, embedding_matrix,
+def build_hierarhical_att_model(MAX_SENTS, MAX_SENT_LENGTH, 
+                         max_vocab, embedding_dim, num_filters,filter_sizes, 
                          num_classes):
     
     # WORDS in one SENTENCE LAYER
@@ -50,25 +62,31 @@ def build_gru_att_model(MAX_SENTS, MAX_SENT_LENGTH,
     sentence_input = Input(shape=(MAX_SENT_LENGTH,), dtype='int32')
     # use embedding_matrix 
 	# (1) embed the words to vectors through an embedding matrix
-    embedded_sequences = Embedding(max_vocab + 1,
-                            embedding_dim,
-                            weights=[embedding_matrix],
-                            input_length=MAX_SENT_LENGTH,
-                            trainable=True)(sentence_input)
+    #embedded_sequences = Embedding(max_vocab + 1,
+    #                       embedding_dim,
+    #                       weights=[embedding_matrix],
+    #                        input_length=MAX_SENT_LENGTH,
+    #                        trainable=True)(sentence_input)
+    embedded_sequences =  Embedding(max_vocab + 1, 
+                   embedding_dim, 
+                   input_length=MAX_SENT_LENGTH, embeddings_regularizer=regularizers.l2(0.0001),
+                   name="embedding")(sentence_input)
 
 	# (2) GRU to get annotations of words by summarizing information
     #     h_it: We obtain an annotation for a given word  by concatenating the forward hidden state  and
     #     backward hidden state
-    l_lstm = Bidirectional(GRU(100, return_sequences=True))(embedded_sequences)
+    sentence_vector = Bidirectional(GRU(50, return_sequences=True))(embedded_sequences)
+    #sentence_vector = conv_layer(embedded_sequences, MAX_SENT_LENGTH, num_filters,filter_sizes)
+    
 	#  Attention layer
 	#  Not all words contribute equally to the representation of the sentence meaning.
 	#  Hence, we introduce attention mechanism to extract such words that are important to the meaning of the
 	#  sentence and aggregate the representation of those informative words to form a sentence vector
 	
 	# (3) u_it: we first feed the word annotation through a one-layer MLP to get the hidden representation u_it
-    l_dense = TimeDistributed(Dense(200))(l_lstm)
+    #l_dense = TimeDistributed(Dense(10))(sentence_vector)
 
-    words_attention_vector = attention_3d_block(l_dense,MAX_SENT_LENGTH) 
+    words_attention_vector = attention_3d_block(sentence_vector,MAX_SENT_LENGTH) 
     #l_att = Flatten()(attention_mul) 
 
 	#  Keras model that process words in one sentence
@@ -83,19 +101,22 @@ def build_gru_att_model(MAX_SENTS, MAX_SENT_LENGTH,
 	# The input should be at least 3D, and the dimension of index one will be considered to be the temporal dimension
 	# Here the sentEncoder is applied to each input record (a note) 
     note_encoder = TimeDistributed(sentEncoder)(note_input)
-    l_lstm_sent = Bidirectional(GRU(100, return_sequences=True))(note_encoder)
+    document_vector = Bidirectional(GRU(50, return_sequences=True))(note_encoder)
+    #document_vector = conv_layer(note_encoder,MAX_SENTS, num_filters,filter_sizes)
+    
 	#attention layer
-    l_dense_sent = TimeDistributed(Dense(200))(l_lstm_sent)
-    sentences_attention_vector = attention_3d_block(l_dense_sent,MAX_SENTS) 
+    #l_dense_sent = TimeDistributed(Dense(250))(document_vector)
+    sentences_attention_vector = attention_3d_block(document_vector,MAX_SENTS) 
     #l_att_sent = Flatten()(attention_mul_sent)
 	# output layer
-    preds = Dense(num_classes, activation='softmax', name='preds')(sentences_attention_vector)
+    preds = Dense(num_classes, activation='sigmoid', name='preds')(sentences_attention_vector)
     
     #model
     model = Model(note_input, preds)
 
-    #model.compile(loss='categorical_crossentropy',optimizer='rmsprop',metrics=['acc'])
-    model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+    #model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=False)
+    model.compile(loss="binary_crossentropy", optimizer="sgd", metrics=["accuracy"])
 
     print("model fitting - Hierachical Attention GRU")
     print model.summary()
--- pipeline/icd9_cnn_att.py
+++ pipeline/icd9_cnn_att.py
@@ -3,13 +3,24 @@ from keras.layers import Dense, Dropout, Flatten, Input, MaxPooling1D, Convoluti
 from keras.layers.merge import Concatenate
 from keras import regularizers
 
-''' code based on:
-https://github.com/alexander-rakhlin/CNN-for-Sentence-Classification-in-Keras/blob/master/sentiment_cnn.py
-http://www.wildml.com/2015/12/implementing-a-cnn-for-text-classification-in-tensorflow/
-https://github.com/dennybritz/cnn-text-classification-tf/blob/master/text_cnn.py
-'''
+def attention_3d_block(inputs):
+    # inputs.shape = (batch_size, time_steps, input_dim)
+    input_dim = int(inputs.shape[2])
+    SEQ_LENGTH =int(inputs.shape[1])
+    # (4) alpah_it: then we measure the importance of x as the similarity of u_it with a x level
+	#     context vector u_w and get a normalized importance weight alpha_it through a softmax function
+    a = Permute((2, 1))(inputs)
+    a = Reshape((input_dim, SEQ_LENGTH))(a)
+    a = Dense(SEQ_LENGTH, activation='softmax')(a)
+    
+	# (5) s_i: After that, we compute the sentence vector s_i as a weighted sum of the word annotations based on the weights alpha_it.
+    a_probs = Permute((2, 1), name='attention_vec')(a)
+    output_attention_mul = merge([inputs, a_probs], name='attention_mul', mode='mul')   
+    sum_vector = Lambda(lambda x: K.sum(x, axis=1))(output_attention_mul)
+ 
+    return sum_vector
 
-def build_icd9_cnn_model(input_seq_length, 
+def build_icd9_cnn_att_model(input_seq_length, 
                          max_vocab, external_embeddings, embedding_dim, embedding_matrix,
                          num_filters, filter_sizes,
                          training_dropout_keep_prob,
@@ -33,30 +44,27 @@ def build_icd9_cnn_model(input_seq_length,
     # Convolutional block
     conv_blocks = []
     for sz in filter_sizes:
-        conv = Convolution1D(filters=num_filters,                         
+        conv = Convolution1D(filters=num_filters,
                          kernel_size=sz,
                          padding="valid",
                          activation="relu",
                          strides=1)(z)
-        window_pool_size =  input_seq_length  - sz + 1 
-        conv = MaxPooling1D(pool_size=window_pool_size)(conv)  
-        conv = Flatten()(conv)
-        conv_blocks.append(conv)
-
+        # here attention layer instead of macPooling 
+        #window_pool_size =  input_seq_length  - sz + 1 
+        #conv = MaxPooling1D(pool_size=window_pool_size)(conv)  
+        #conv = Flatten()(conv)
+        #conv_blocks.append(conv)
+        features_attention_vector = attention_3d_block(conv)
     #concatenate
     z = Concatenate()(conv_blocks) if len(conv_blocks) > 1 else conv_blocks[0]
     z = Dropout(training_dropout_keep_prob)(z)
 
     #score prediction
-    #z = Dense(num_classes, activation="relu")(z)  I don't think this is necessary
-    #model_output = Dense(num_classes, activation="softmax")(z)
     model_output = Dense(num_classes, activation="sigmoid")(z)
 
     #creating model
     model = Model(model_input, model_output)
-    # what to use for tf.nn.softmax_cross_entropy_with_logits?
     model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
-    #model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
     
     print model.summary()
 
